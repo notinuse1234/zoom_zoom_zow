f2021177aa219833bf80830a57b514480a4be90a
Austin Hester <ahester@cisco.com>
2019-05-01 17:09:46 -0400

    cleanup the golf club
    Signed-off-by: Austin Hester <ahester@cisco.com>

--- objects/golf_club.py
+++ objects/golf_club.py
@@ -16,15 +16,12 @@ class GolfClub(pg.sprite.Sprite):
         self.size = 100, 100
         # Set the radius for detecting collisions
         self.radius = 50
-        self.set_resting()
         # Set the rectangle, or the box where it is drawn
-        self.rect = self.surf.get_rect()
         self.original_bottom_loc = self.screen.get_size()[1] - 20
         self.original_right_loc = self.screen.get_size()[0] / 2 - 30
-        self.rect.bottom = self.original_bottom_loc
-        self.rect.right = self.original_right_loc
         self.vector = [0, 0]
         self.speed = 0
+        self.set_resting()
 
     def set_image(self, image_name):
         # Set the image and set it to the defined size 
@@ -34,18 +31,28 @@ class GolfClub(pg.sprite.Sprite):
             ).convert_alpha(),
             self.size
         )
+        if not hasattr(self, 'rect'):
+            self.rect = self.surf.get_rect()
 
     def set_resting(self):
         self.set_image("golf_club.png")
+        self.rect.right = self.original_right_loc
+        self.rect.bottom = self.original_bottom_loc
 
     def set_mid_swing(self):
         self.set_image("golf_club_mid.png")
+        self.rect.right = self.original_right_loc - 100
+        self.rect.bottom = self.original_bottom_loc
 
     def set_full_swing(self):
         self.set_image("golf_club_full.png")
+        self.rect.right = self.original_right_loc - 110
+        self.rect.bottom = self.original_bottom_loc - 120
 
     def set_followthru(self):
         self.set_image("golf_club_followthru.png")
+        self.rect.right = self.original_right_loc - 110
+        self.rect.bottom = self.original_bottom_loc - 120
 
     def update(self, pressed_keys):
         """Update the golf club's location.
@@ -56,23 +63,15 @@ class GolfClub(pg.sprite.Sprite):
         if pressed_keys[pg.K_UP]:
             self.vector[1] = -self.speed
             self.set_full_swing()
-            self.rect.right = self.original_right_loc - 110
-            self.rect.bottom = self.original_bottom_loc - 120
         if pressed_keys[pg.K_DOWN]:
             self.vector[1] = self.speed
             self.set_resting()
-            self.rect.right = self.original_right_loc
-            self.rect.bottom = self.original_bottom_loc
         if pressed_keys[pg.K_LEFT]:
             self.vector[0] = -self.speed
             self.set_followthru()
-            self.rect.right = self.original_right_loc - 110
-            self.rect.bottom = self.original_bottom_loc - 120
         if pressed_keys[pg.K_RIGHT]:
             self.vector[0] = self.speed
             self.set_mid_swing()
-            self.rect.right = self.original_right_loc - 100
-            self.rect.bottom = self.original_bottom_loc
         # Actually move the ball on the screen
         self.rect.move_ip(*self.vector)
         # Keep ball on the screen
