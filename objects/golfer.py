3f3de2e99752795db32a25b31d874ebc683e2c5d
Austin Hester <ahester@cisco.com>
2019-05-01 16:58:02 -0400

    make the golfer and club change together
    Signed-off-by: Austin Hester <ahester@cisco.com>

--- objects/golfer.py
+++ objects/golfer.py
@@ -16,19 +16,34 @@ class Golfer(pg.sprite.Sprite):
         self.size = 110, 200
         # Set the radius for detecting collisions
         self.radius = 50
-        # Set the image and set it to the defined size 
-        self.surf = pg.transform.scale(
-            pg.image.load(
-                os.path.join(os.getcwd(), "resources", "golfer.png")
-            ).convert_alpha(),
-            self.size
-        )
+        self.set_resting()
         # Set the rectangle, or the box where it is drawn
         self.rect = self.surf.get_rect()
         self.rect.bottom = self.screen.get_size()[1] - 20
         self.rect.right = self.screen.get_size()[0] / 2 - 75
         self.vector = [0, 0]
         self.speed = 0
+
+    def set_image(self, image_name):
+        # Set the image and set it to the defined size
+        self.surf = pg.transform.scale(
+            pg.image.load(
+                os.path.join(os.getcwd(), "resources", image_name)
+            ).convert_alpha(),
+            self.size
+        )
+
+    def set_resting(self):
+        self.set_image("golfer.png")
+
+    def set_mid_swing(self):
+        self.set_image("golfer_mid.png")
+
+    def set_full_swing(self):
+        self.set_image("golfer_full.png")
+
+    def set_followthru(self):
+        self.set_image("golfer_followthru.png")
 
     def update(self, pressed_keys):
         """Update the golfer's location.
@@ -38,12 +53,16 @@ class Golfer(pg.sprite.Sprite):
         self.vector = [0, 0]
         if pressed_keys[pg.K_UP]:
             self.vector[1] = -self.speed
+            self.set_full_swing()
         if pressed_keys[pg.K_DOWN]:
             self.vector[1] = self.speed
+            self.set_resting()
         if pressed_keys[pg.K_LEFT]:
             self.vector[0] = -self.speed
+            self.set_followthru()
         if pressed_keys[pg.K_RIGHT]:
             self.vector[0] = self.speed
+            self.set_mid_swing()
         # Actually move the ball on the screen
         self.rect.move_ip(*self.vector)
         # Keep ball on the screen
