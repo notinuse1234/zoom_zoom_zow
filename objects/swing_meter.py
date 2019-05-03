d6044ea4b0d5dd0cb372adb905b58f20a6df5354
Austin Hester <ahester@cisco.com>
2019-05-03 15:54:39 -0400

    give swing meter some booleans and integers
    Signed-off-by: Austin Hester <ahester@cisco.com>

--- objects/swing_meter.py
+++ objects/swing_meter.py
@@ -18,43 +18,50 @@ class SwingMeter():
         self.screen = screen
         self.size = 500, 20
         # Set the rectangle, or the box where it is drawn
-        self.original_bottom_loc = self.screen.get_size()[0] // 2 - 300
-        self.original_right_loc = self.screen.get_size()[1] - 40
+        self.x = self.screen.get_size()[0] // 2 - 300
+        self.y = self.screen.get_size()[1] - 40
         # Objects
         self.golfer = golfer
+        # Booleans
+        self.in_swing = False
+        self.in_backswing = False
+        self.in_followthru = False
+        # Integers
+        self.power = 0
+        self.accuracy = 0
+        self.perfect_power = self.x
+        self.perfect_accuracy = self.x + self.size[0] - (self.size[0] / 5)
 
     def update(self, pressed_keys):
         """The swing meter."""
-        x = self.original_bottom_loc
-        y = self.original_right_loc
         w, h = self.size
         # draw the black outline
         pg.draw.rect(
             self.screen,
             Colors.get('black'),
-            (x-2, y-2, w+4, h+4)
+            (self.x-2, self.y-2, w+4, h+4)
         )
         # draw the gray box
         pg.draw.rect(
             self.screen,
             Colors.get('darkdarkgray'),
-            (x, y, w, h)
+            (self.x, self.y, w, h)
         )
         if self.golfer.is_in_swing:
             pg.draw.rect(
                 self.screen,
                 Colors.get('water'),
-                (x, y, w, h)
+                (self.x, self.y, w, h)
             )
         # draw each bar
-        for i in range(x, x+w, 10):
-            if i == x + w - (w / 5):
+        for i in range(self.x, self.x+w, 10):
+            if i == self.perfect_accuracy:
                 color = Colors.get('flag')
             else:
                 color = Colors.get('darkgray')
             pg.draw.rect(
                 self.screen,
                 color,
-                (i+2, y+2, 6, h-2)
+                (i+2, self.y+2, 6, h-2)
             )
 
